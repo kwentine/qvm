6b1cd64ecdc97dd70521cf788c8aeed4f185058a
Quentin Ågren <quentin.agren@gmail.com>
2025-12-24 12:29:33 +0100

    Simplify env file

--- env.sh
+++ env.sh
@@ -1,11 +1,10 @@
 # Adjust depending on your setup
-repo_root=$(git rev-parse --show-toplevel)
 qvm_dir=var/vms
 # Toolbox container
 if [[ -f /run/.containerenv ]] && [[ -d /run/host ]]; then
-  root_dir=/run/host
+  fs_root=/run/host
 fi
-export QVM_DIR="${root_dir:-}/${qvm_dir}"
+QVM_DIR="${fs_root:-}/${qvm_dir}"
 PATH="$(pwd)/scripts:${PATH}"
-export PATH
-export KUBECONFIG="${repo_root}/k8s/admin.kubeconfig:${repo_root}/rke2/kubeconfig.yaml"
+KUBECONFIG="${QVM_DIR}/kubeconfigs/admin.yaml:${QVM_DIR}/rke2.yaml"
+export PATH QVM_DIR KUBECONFIG
